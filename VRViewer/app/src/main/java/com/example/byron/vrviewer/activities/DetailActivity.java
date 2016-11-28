4af436d1074ea57c8dfecab11d9f6c96ef7af5fd
Byron Ajin <byronap120@gmail.com>
2016-11-27 18:58:29 -0600

    DetailActivity

--- VRViewer/app/src/main/java/com/example/byron/vrviewer/activities/DetailActivity.java
+++ VRViewer/app/src/main/java/com/example/byron/vrviewer/activities/DetailActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.byron.vrviewer.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements ValueEventListener {
 
     private DatabaseReference mPostReference;
     private String postRef;
@@ -20,5 +22,15 @@ public class DetailActivity extends AppCompatActivity {
 
         postRef = getIntent().getStringExtra("postRef");
         mPostReference = FirebaseDatabase.getInstance().getReference().child("posts").child(postRef);
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
     }
 }
